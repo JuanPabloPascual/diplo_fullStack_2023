300af6ec4abfbc9ec5b325a08180876f214e2b79
Juan Pablo Pascual <jppascualiramain@gmail.com>
2023-12-11 20:52:56 -0300

    Curso_UTN

---
+++ clase8_script.js
@@ -0,0 +1,40 @@
+let nombre = "Lucas";
+let numero = 10;
+let boolan = false;
+
+// console.log(nombre)
+
+let num1 = 1;
+let num2 = 2;
+let resultado = num1 + num2
+
+// console.log(resultado)
+
+let comp = 4 !== "4";
+// console.log(comp)
+
+let modulo = 10 % 3
+// console.log(modulo)
+
+
+let comp1 = (2 > 2) || (2 > 2)
+// console.log(comp1)
+
+let frutas = ["manzanas", "peras", "frutilla"]
+//console.log(frutas[0])
+
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 35
+}
+
+// console.log(persona)
+
+const productos = 
+    {
+    "nombre": 'Manzana',
+    "precio": 1.5,
+    "cantidad": 8,
+    }
+
