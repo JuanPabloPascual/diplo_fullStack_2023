177e47305483d0cf1e8516a06e1c90b290e2a649
Juan Pablo Pascual <jppascualiramain@gmail.com>
2023-12-13 20:48:11 -0300

    Curso_UTN

---
+++ script.js
@@ -0,0 +1,70 @@
+// Condicionales
+
+// let edad = 16;
+
+// if (edad >=18) {
+//    console.log("Sos mayor de edad")
+// } 
+// else if(edad >= 18){
+//    console.log("Eres mayuor de edad pero menos a 21 anios")
+// }
+// else{
+//    console.log("Aun no eres mayor de edad");
+// } 
+
+// Switch
+
+// let diaDeLaSemana = "Lunes";
+
+// switch (diaDeLaSemana) {
+//     case 'Domingo':
+//      console.log('Hoy es domingo');
+//       break;
+
+//       case 'Viernes':
+//          console.log('Hoy es viernes');
+//            break;
+
+//       default:
+//           console.log("Es otro dia")
+
+
+
+// Operador Ternario
+
+// let edadd = 17;
+// let esMayor = (edad >= 18) ? true : false;
+// console.log(esMayor)
+
+
+            // Funciones
+//
+// function saludar(nombre){
+//    console.log("Hola," + nombre + "!")
+// }
+
+// saludar("Juan")
+
+// const saludar = nombre =>{
+//        console.log(`Hola, ${nombre}`)
+// }
+
+// saludar("Juan")
+
+// function suma(num1, num2){
+//    return num1 + num2;
+//}
+
+// let resultado = suma (5, 3);
+//       console.log("La suma total es : + resultado");
+
+// function esMayorDeEdad(edad){
+//    return(edad >= 18) ? true : false
+// }
+
+// function mostrarMensajeEdad(edad){
+//     console.log(esMayorDeEdad (edad) ? "Sos mayor de edad" : "Sos menor de edad")
+// }
+
+// mostrarMensajeEdad(12)
+
