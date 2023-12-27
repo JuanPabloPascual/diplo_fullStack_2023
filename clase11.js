81b1a790e10ca0b6fd977f9c2e87216a885d2900
Juan Pablo Pascual <jppascualiramain@gmail.com>
2023-12-27 19:48:12 -0300

    Curso_UTN

---
+++ clase11.js
@@ -0,0 +1,17 @@
+// Hicimos 3 ejercicios de repaso //
+
+// Bucles FOR y WHILE
+
+// por buena practica se usa la letra i de iterar pero podria ir cualquier cosa 
+
+
+// for (let i = 1; i <= 5; i++){
+//    console.log(i)
+// }
+
+
+let contador = 1
+while(contador <= 5 ){
+    console.log(contador)
+    contador++
+}
