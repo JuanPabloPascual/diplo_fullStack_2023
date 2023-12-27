3c8561a761c0d889cda2c93e791d1e8ab0070a1e
Juan Pablo Pascual <jppascualiramain@gmail.com>
2023-12-27 20:56:42 -0300

    Curso_UTn

---
+++ clase_ejemplo.js
@@ -0,0 +1,13 @@
+
+
+// let primerParrafo = document.querySelector('p')  // Asi manipulo la etiqueta del HTML de prarafo
+
+const h1Element = document.querySelector('h1')
+
+const pEelement = document.querySelector(".mi-parrafo")
+
+console.log(h1Element)
+
+console.log(pEelement)
+
+console.log(h1Element.textContent)
--- clases_com2/clase_11/clase11.js
+++ clases_com2/clase_11/clase11.js
@@ -10,8 +10,8 @@
 // }
 
 
-let contador = 1
-while(contador <= 5 ){
-    console.log(contador)
-    contador++
-}
+// let contador = 1
+// while(contador <= 5 ){
+//    console.log(contador)
+//    contador++
+// }
